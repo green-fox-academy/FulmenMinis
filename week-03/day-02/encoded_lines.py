9247ab0c2c5d6d7dc1cb8d75dd88560a905084ad
FulmenMinis <erdos.horvath.emese@gmail.com>
2017-12-06 03:39:24 +0100

    Optional Hard Exercise10 done

--- week-03/day-02/encoded_lines.py
+++ week-03/day-02/encoded_lines.py
@@ -1,3 +1,40 @@
 # Create a method that decrypts encoded-lines.txt
+# Solution1
 def decrypt(file_name):
-    pass
+    orig_file = open(file_name, "r")
+    new_file = open("encoded.txt", "w")
+    lines = orig_file.readlines()
+    for line in lines:
+        string = ""
+        for char in line:
+            number = ord(char)
+            if number != 32:
+                temporary = chr(number - 1)
+                string += temporary
+            else:
+                string += " "
+        new_file.writelines(string + "\n")
+    new_file.writelines(string + "\n")
+    print("Finished!")
+decrypt("encoded-lines.txt")
+
+#Solution2
+def decrypt(file_name):
+    try:
+        orig_file = open(file_name, "r")
+        text = orig_file.readlines()
+        orig_file.close()
+        new_file = open("encoded.txt", "w")
+        for line in text:
+            new_text = ""
+            for i in range(len(line) -1):
+                if line[i] == " ":
+                    new_text += " "
+                else:
+                    new_text += chr(ord(line[i]) -1)
+            new_text += '\n'
+            new_file.writelines(new_text)
+        new_file.close()
+    except IOError:
+        return "Unable to write file."
+decrypt("encoded-lines.txt")
